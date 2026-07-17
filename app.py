a9b15c5dc161dce21231f4431cd920229a9d533a
Hope Mutindi <msndindi@outlook.com>
2026-07-17 14:01:26 +0200

    Find if customer is eligible/not

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ print("bag" > "apple")
 
 
 # conditional statements
-#indenting and : are very important in conditional statements
+# indenting and : are very important in conditional statements
 
 temperature = 15
 if temperature > 25:
@@ -28,3 +28,50 @@ elif temperature < 10:
 else:
     print("It's a cold day")
 print("Done")
+
+
+# Ternary operator
+
+# applicants_age = 20
+# if applicants_age >= 18:
+#     print("Applicant is eligible to vote")
+# else:
+#     print("Wait until you are 18 years old to vote")
+
+
+applicants_age = 20
+message = "Applicant is eligible to vote" if applicants_age >= 18 else "Wait until you are 18 years old to vote"
+print(message)
+
+
+student_age = 25
+message = "You are qualified to apply for tech program" if student_age >= 20 else "Wait until you turn 18 years old to apply for tech program"
+print(message)
+
+# logical operators : and, or, not
+
+high_income = True
+good_credit = False
+student = True
+
+#and operator executes if both conditions are true
+#or operator executes if any one of the conditions is true
+#not operator executes if the condition is false
+
+# if high_income or good_credit:
+#     print("Eligible for loan")
+# else:
+#     print("Not eligible for loan")
+
+
+# if not student:
+#     print("Eligible for loan")
+# else:
+#     print("Not eligible for loan")
+
+#You can combine logical operators to create more complex conditions. For example, you can check if a person is eligible for a loan based on their income, credit score, and student status.
+
+if (high_income or good_credit) and not student:
+    print("Eligible for loan")
+else:
+    print("Not eligible for loan")
