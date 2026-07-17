575ea9e2d926843d510aa07f566431252689d9c7
Hope Mutindi <msndindi@outlook.com>
2026-07-17 13:25:13 +0200

    Conditional statement

---
+++ app.py
@@ -0,0 +1,29 @@
+# comparison operators
+
+print(10 == 10)
+print(10 != 10)
+print(10 < 10)
+print(10 > 10)
+print(10 <= 10)
+print(10 >= 10)
+
+
+ord('a')
+print(ord('a'))
+
+
+"bag" > "apple"
+print("bag" > "apple")
+
+
+# conditional statements
+
+temperature = 15
+if temperature > 25:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature < 10:
+    print("It's a nice day")
+else:
+    print("It's a cold day")
+print("Done")
